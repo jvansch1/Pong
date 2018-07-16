5e78a08fe809011a8a8e7bde940f448b482a16f0
johnvanschultz <jvansch1@binghamton.edu>
2018-07-15 20:10:10 -0400

    Refactor ball class to handle position internally

--- ball.py
+++ ball.py
@@ -5,6 +5,31 @@ class Ball():
     def __init__(self, surface, radius):
         self.radius = radius
         self.surface = surface
+        self.x = 500
+        self.y = 500
+        self.ball_x_delta = 20
+        self.ball_y_delta = 20
 
-    def draw_ball(self, x, y):
-        pygame.draw.circle(self.surface, (255,255,255), (x, y), 10)
+
+    def draw_ball(self):
+        self.handle_x_pos()
+        self.handle_y_pos()
+        pygame.draw.circle(self.surface, (255,255,255), (self.x, self.y), 10)
+
+    def handle_x_pos(self):
+        if self.x > (980):
+            self.ball_x_delta = -20
+
+        if self.x < 20:
+            self.ball_x_delta += 20
+
+        self.x += self.ball_x_delta
+
+    def handle_y_pos(self):
+        if self.y > (780):
+            self.ball_y_delta = -20
+
+        if self.y < 20:
+            self.ball_y_delta += 20
+
+        self.y += self.ball_y_delta
--- main.py
+++ main.py
@@ -30,15 +30,10 @@ def main():
     surface = pygame.Surface((5, height))
     surface.fill(white)
 
-    ball_x = int(width / 2)
-    ball_y = int(height / 2)
-    ball_x_delta = 20
-    ball_y_delta = 15
-
     ball = Ball(screen.screen, 10)
 
-    paddle_one = Paddle(screen, 20, paddle_one_y)
-    paddle_two = Paddle(screen, 960, paddle_two_y)
+    paddle_one = Paddle(screen, 20, 50)
+    paddle_two = Paddle(screen, 960, 50)
 
     screen.add_with_blit(surface, width / 2, 0)
 
@@ -52,22 +47,8 @@ def main():
         paddle_one.draw_paddle()
         paddle_two.draw_paddle()
         screen.add_with_blit(surface, width / 2, 0)
-        ball_x += ball_x_delta
-        ball_y += ball_y_delta
 
-        if ball_x > (width - 20):
-            ball_x_delta = -20
-
-        if ball_x < 20:
-            ball_x_delta = 20
-
-        if ball_y > (height - 20):
-            ball_y_delta = -15
-
-        if ball_y < 20:
-            ball_y_delta = 15
-
-        ball.draw_ball(ball_x, ball_y)
+        ball.draw_ball()
         pygame.display.update()
 
 main()
